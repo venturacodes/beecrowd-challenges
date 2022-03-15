5c3646fb48c03088b2aeb87a65d1a5ca40eee85d
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-15 08:48:26 -0300

    Resolvido exercício 1022

--- 1022.php
+++ 1022.php
@@ -1,17 +1,16 @@
 <?php
-/*
 
-Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+$entrada = fgets(STDIN);
+$array = explode(" ", $entrada);
+$a = $array[0];
+$b = $array[1];
+$c = $array[2];
+$d = $array[3];
 
-Entrada
-Quatro números inteiros A, B, C e D.
-
-Saída
-Mostre a respectiva mensagem após a validação dos valores.
-
-Exemplo de Entrada	      Exemplo de Saída
-5 6 7 8                   Valores nao aceitos
-
-2 3 2 6                   Valores aceitos
-*/
+if(($b > $c) && ($d > $a) && (($c+$d) > ($a +$b)) && ($c > 0 && $d > 0) && ($a % 2 == 0)){
+    echo "Valores aceitos\n";
+}else{
+    echo "Valores nao aceitos\n";
+}
+?>
 
