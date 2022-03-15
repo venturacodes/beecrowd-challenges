57f4909116c5eb3713f7e6dd45ce0df1297d3494
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-15 09:43:42 -0300

    Concluído exercicio 1024

--- 1024.php
+++ 1024.php
@@ -1,1 +1,14 @@
-1024.php
+<?php
+
+$entrada = fgets(STDIN);
+
+if(($entrada >= 0) && ($entrada <= 25))
+    echo "Intervalo [0,25]\n";
+elseif(($entrada > 25) && ($entrada <= 50))
+    echo "Intervalo (25,50]\n";
+elseif(($entrada > 50) && ($entrada <= 75))
+    echo "Intervalo (50,75]\n";
+elseif(($entrada > 75) && ($entrada <= 100))
+    echo "Intervalo (75,100]\n";
+else
+    echo "Fora de intervalo\n";
