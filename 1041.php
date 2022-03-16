ecd0f78ab978c231306c920e0296fb4e7a736ad5
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-16 14:13:09 -0300

    1041 resolvido

--- 1041.php
+++ 1041.php
@@ -1,1 +1,19 @@
 <?php
+
+list($x, $y) = explode(" ", readline());
+
+if($x+$y==0)
+    echo "Origem\n";
+elseif($x==0)
+    echo "Eixo Y\n";
+elseif($y==0)
+    echo "Eixo X\n";
+elseif($x > 0 && $y > 0)
+    echo "Q1\n";
+elseif($x < 0 && $y > 0)
+    echo "Q2\n";
+elseif($x < 0 && $y < 0)
+    echo "Q3\n";
+elseif($x > 0 && $y < 0)
+    echo "Q4\n";
+?>
