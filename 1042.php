3af4551027f7a138c78b0c9328acbfb29daeaf40
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-16 14:41:04 -0300

    1042 resolvido

--- 1042.php
+++ 1042.php
@@ -1,3 +1,14 @@
 <?php
 
+$array = explode(" ", readline());
+$arrayOriginal = $array;
+sort($array, SORT_NUMERIC);
+foreach($array as $item){
+    echo $item."\n";
+}
+echo "\n";
+foreach($arrayOriginal as $item){
+    echo $item."\n";
+}
+
 ?>
