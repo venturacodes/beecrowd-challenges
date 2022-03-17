90daa21345f61963067359f2d5fde4474424585c
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-17 09:05:08 -0300

    1070 resolvido

--- 1070.php
+++ 1070.php
@@ -1,3 +1,12 @@
 <?php
-    
+    $entrada = readline();
+    const LIMITE_VALORES_IMPARES = 6;
+    $countImpar = 0;
+    while($countImpar < LIMITE_VALORES_IMPARES){
+        if($entrada % 2 !== 0){
+            echo "$entrada\n";
+            $countImpar++;
+        }
+        $entrada++;
+    }
 ?>
