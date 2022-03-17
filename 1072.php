2ed0ee25ec5ce2a648db2304892ff1cbd6fecb92
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-17 08:58:37 -0300

    1072 resolvido

--- 1072.php
+++ 1072.php
@@ -1,4 +1,23 @@
 <?php
 
+$qtdLidos = readline();
+$i = 0;
+while($qtdLidos > $i){
+    $lidos[$i] = readline();
+    $i++;
+}
+
+$intervalo = [10,20];
+$countIn  = 0;
+$countOut = 0;
+foreach($lidos as $lido){
+    if(($lido >= $intervalo[0]) && ($lido <= $intervalo[1])){
+        $countIn++;
+    }else{
+        $countOut++;
+    }
+}
+echo "$countIn in\n";
+echo "$countOut out\n";
 
 ?>
