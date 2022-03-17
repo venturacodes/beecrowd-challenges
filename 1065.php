c886e82dcd09cf9739fdb7b49bdf3c3a8e41eab7
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-17 08:32:42 -0300

    1065 resolvido

--- 1065.php
+++ 1065.php
@@ -1,4 +1,20 @@
 <?php
 
+$entrada1 = fgets(STDIN);
+$entrada2 = fgets(STDIN);
+$entrada3 = fgets(STDIN);
+$entrada4 = fgets(STDIN);
+$entrada5 = fgets(STDIN);
+
+$array = [$entrada1, $entrada2, $entrada3, $entrada4, $entrada5];
+
+$countPar = 0;
+foreach($array as $item){
+    if($item % 2 === 0){
+        $countPar++;
+    }
+}
+
+echo "$countPar valores pares\n";
 
 ?>
