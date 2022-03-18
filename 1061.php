479ee680e4441a8e50d8b7bfb231ae0330fd2eea
venturacodes <arthur.alves.venturin@gmail.com>
2022-03-18 17:33:07 -0300

    Não consegui resolver, repensar lógica

---
+++ 1061.php
@@ -0,0 +1,20 @@
+<?php
+
+    list($x, $DiaInicio) = explode(" ", readline());
+    list($horaInicio, $minutoInicio, $segundoInicio) =  explode(" : ",readline());
+    list($x, $DiaFim) = explode(" ", readline());
+    list($horaFim, $minutoFim, $segundoFim) =  explode(" : ",readline());
+    if($DiaInicio - $DiaFim == 0){
+        $dia = 0;
+        $hora = $horaFim > $horaInicio ? ($horaFim - $horaInicio)  : ($horaInicio - $horaFim);
+    }else{
+        $dia = $DiaFim > $DiaInicio ? ($DiaFim - 1) -$DiaInicio : ($DiaInicio - 1) -$DiaFim;
+        $hora = $horaFim > $horaInicio ? 24 -($horaFim - $horaInicio)  : 24 - ($horaInicio - $horaFim);
+    }
+    $minuto = $minutoFim > $minutoInicio ? ($minutoFim - $minutoInicio)  : ($minutoInicio - $minutoFim);
+    $segundo = $segundoFim > $segundoInicio ? ($segundoFim - $segundoInicio)  : ($segundoInicio - $segundoFim);
+    echo "$dia dia(s)\n";
+    echo "$hora hora(s)\n";
+    echo "$minuto minuto(s)\n";
+    echo "$segundo segundos(s)\n";
+?>
